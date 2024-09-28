3046c7f66d9e557875fc42967a3a7104d3087948
Jamshid Khaytbaev <itsjk0307@gmail.com>
2024-09-29 02:55:48 +0900

    '.'

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -4,6 +4,25 @@ import "./Experience.css";
 const Experience = () => {
   return (
     <div className="experience-container">
+      {/* Experience as an Intern */}
+      <div className="experience-card">
+        <h3 className="experience-title">
+          Frontend Developer Intern · RainbowTech
+        </h3>
+        <span className="experience-duration">March 2023 — July 2023</span>
+        <p className="experience-description">
+          Assisted in building and maintaining user interfaces for various web
+          projects. Worked closely with senior developers to learn best
+          practices and understand key aspects of frontend development.
+        </p>
+        <div className="skills">
+          <span className="skill-tag">HTML</span>
+          <span className="skill-tag">CSS</span>
+          <span className="skill-tag">JavaScript</span>
+          <span className="skill-tag">Version Control (Git)</span>
+          <span className="skill-tag">Responsive Design</span>
+        </div>
+      </div>
       {/* Experience as a Frontend Developer Researcher */}
       <div className="experience-card">
         <h3 className="experience-title">
@@ -25,30 +44,10 @@ const Experience = () => {
         </div>
       </div>
 
-      {/* Experience as an Intern */}
-      <div className="experience-card">
-        <h3 className="experience-title">
-          Frontend Developer Intern · RainbowTech
-        </h3>
-        <span className="experience-duration">March 2023 — July 2024</span>
-        <p className="experience-description">
-          Assisted in building and maintaining user interfaces for various web
-          projects. Worked closely with senior developers to learn best
-          practices and understand key aspects of frontend development.
-        </p>
-        <div className="skills">
-          <span className="skill-tag">HTML</span>
-          <span className="skill-tag">CSS</span>
-          <span className="skill-tag">JavaScript</span>
-          <span className="skill-tag">Version Control (Git)</span>
-          <span className="skill-tag">Responsive Design</span>
-        </div>
-      </div>
-
       {/* View Full Résumé Link */}
       <div className="resume-link">
         <a
-          href="[Your Resume Link Here]"
+          href="https://drive.google.com/file/d/1um1zw9LT1Zi8kQs-EYhjWdFkmR5xjYU6/view?usp=drive_link"
           target="_blank"
           rel="noopener noreferrer"
         >
